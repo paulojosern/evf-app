3e5facbf47141b8584138a7a89b1594f230869c6
“Paulo <paulojrn82@gmail.com>
2020-07-23 19:48:43 -0300

    ajuste reservation context hide

--- pages/condominio/reservations.context.js
+++ pages/condominio/reservations.context.js
@@ -1,54 +1,54 @@
-import React, { createContext, useContext, useState } from 'react';
+// import React, { createContext, useContext, useState } from 'react';
 
-const ReservationsContext = createContext({});
+// const ReservationsContext = createContext({});
 
-const ReservationsProvider = ({ children }) => {
-	const [panelState, setPanelState] = useState();
-	const [panelCategories, setPanelCategories] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [confirm, setConfirm] = useState({
-		active: false,
-		msg: '',
-		function: null,
-	});
+// const ReservationsProvider = ({ children }) => {
+// 	const [panelState, setPanelState] = useState();
+// 	const [panelCategories, setPanelCategories] = useState([]);
+// 	const [loading, setLoading] = useState(false);
+// 	const [confirm, setConfirm] = useState({
+// 		active: false,
+// 		msg: '',
+// 		function: null,
+// 	});
 
-	const inputStatePanel = (payload) => {
-		setPanelState({ ...payload });
-	};
+// 	const inputStatePanel = (payload) => {
+// 		setPanelState({ ...payload });
+// 	};
 
-	const inputCategories = (payload) => {
-		setPanelCategories([...payload]);
-	};
+// 	const inputCategories = (payload) => {
+// 		setPanelCategories([...payload]);
+// 	};
 
-	const showLoading = (value) => {
-		setLoading(value);
-	};
+// 	const showLoading = (value) => {
+// 		setLoading(value);
+// 	};
 
-	const showConfirm = (payload) => {
-		setConfirm({ ...payload });
-	};
+// 	const showConfirm = (payload) => {
+// 		setConfirm({ ...payload });
+// 	};
 
-	return (
-		<ReservationsContext.Provider
-			value={{
-				panelState,
-				inputStatePanel,
-				panelCategories,
-				inputCategories,
-				loading,
-				showLoading,
-				confirm,
-				showConfirm,
-			}}
-		>
-			{children}
-		</ReservationsContext.Provider>
-	);
-};
+// 	return (
+// 		<ReservationsContext.Provider
+// 			value={{
+// 				panelState,
+// 				inputStatePanel,
+// 				panelCategories,
+// 				inputCategories,
+// 				loading,
+// 				showLoading,
+// 				confirm,
+// 				showConfirm,
+// 			}}
+// 		>
+// 			{children}
+// 		</ReservationsContext.Provider>
+// 	);
+// };
 
-const useReservationsContext = () => {
-	const context = useContext(ReservationsContext);
-	return context;
-};
+// const useReservationsContext = () => {
+// 	const context = useContext(ReservationsContext);
+// 	return context;
+// };
 
-export { useReservationsContext, ReservationsProvider };
+// export { useReservationsContext, ReservationsProvider };
